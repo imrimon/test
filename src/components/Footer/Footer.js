efef07dbcb5e869d9177fbd563f61d4e574f3112
imrimon <72396280+imrimon@users.noreply.github.com>
2021-10-04 23:29:36 +0600

    Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="bg-primary">
             <div className="row d-flex">
                 <div className="col-lg-12 col-sm-12  mt-2 mb-2 text-center text-white">
-                    Copyright © 2021 eLearning All rights reserved.
+                    Copyright © 2021 || SmartEDU All rights reserved.
                 </div>
             </div>
         </div>
